c697d824f32133ee30c5f613cc269e0d2d7abf5b
Robin Novak <rnovak@g.clemson.edu>
2014-04-06 18:54:10 -0400

    Best Version Yet
    Pulls points and names from external DB
    Prints names on geoObjects which face the camera

--- src/com/example/demo/MainActivity.java
+++ src/com/example/demo/MainActivity.java
@@ -4,6 +4,9 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,6 +17,8 @@ import gl.GL1Renderer;
 import gl.GLFactory;
 import gl.Color;
 import gl.GLText;
+import gl.animations.AnimationFaceToCamera;
+import gl.scenegraph.MeshComponent;
 import gl.scenegraph.Shape;
 import system.ArActivity;
 import system.DefaultARSetup;
@@ -31,6 +36,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.text.DynamicLayout;
 import android.text.Layout.Alignment;
 import android.text.StaticLayout;
 import android.text.TextPaint;
@@ -50,6 +56,7 @@ public class MainActivity extends Activity {
 	double y;
 	
 	ArrayList<Double> LatList, LonList;
+	ArrayList<String> PList;
 	private ProgressDialog pDialog;
 	
 	@Override
@@ -64,6 +71,7 @@ public class MainActivity extends Activity {
 		
 		LonList = new ArrayList<Double>();
 		LatList = new ArrayList<Double>();
+		PList = new ArrayList<String>();
 		
 		b.setOnClickListener(new OnClickListener() {
 
@@ -96,6 +104,13 @@ public class MainActivity extends Activity {
 						
 								ArrayList<Location> locationList = new ArrayList<Location>();
 							    
+								//Location test = (newLoc(34.725244, -82.909956));
+								
+								// add points from external db
+								for(int i=0; i<PList.size(); i++){
+									locationList.add(newLoc(LatList.get(i),LonList.get(i)));
+								}
+								
 								/*
 							    // Locations near Robin's House
 							    locationList.add(newLoc(34.725244, -82.909956));
@@ -103,7 +118,9 @@ public class MainActivity extends Activity {
 							    locationList.add(newLoc(34.724948, -82.909795));
 							    locationList.add(newLoc(34.724997, -82.909607));
 							    locationList.add(newLoc(34.72559, -82.907668));
+							    */
 							    
+							    /*
 							    // McAdams
 							    locationList.add(newLoc(34.6755916, -82.8346138));
 							    
@@ -135,23 +152,32 @@ public class MainActivity extends Activity {
 							    
 							    // Martin Hall
 							    locationList.add(newLoc(34.6780583, -82.8355305));
+							    
 							    */
 							    
-								Toast toast = Toast.makeText(getApplicationContext(), "LatList.size(): " + Integer.toString(LatList.size()), 10000);
-						    	toast.show();
+							//	Toast toast = Toast.makeText(getApplicationContext(), "PList.size(): " + Integer.toString(PList.size()) + "\n CONTENTS: " + PList.get(0), 10000);
+						    	  
+							//	Toast toast = Toast.makeText(getApplicationContext(), "LatList.size(): " + Integer.toString(LatList.size()), 10000);
+						    //	toast.show();
 								
 								for(int i = 0; i < LatList.size(); i++){
 									locationList.add(newLoc(LatList.get(i), LonList.get(i)));
 								}
 								
-							    
+							    String text = "";
 							    Obj x;
-							    for(Location i: locationList){
-							    	x = new GeoObj(i);
+							    //for(Location i: locationList){
+							    for(int i = 0; i < PList.size(); i++){
+							    	x = new GeoObj(locationList.get(i));
 							    	//x.setComp(objectFactory.newCube(Color.red()));
 							    	//x.setComp(objectFactory.newTextured2dShape(textAsBitmap("TEST", (float)18.0, Color.red().toIntRGB()), "TESTBITMAP"));
 							    	
-							    	String text = "Hello world!";
+							    	//String text = "Hello world!";
+							    	text = PList.get(i);
+							    	Log.i("PLISTTEST","PList.size(): " + Integer.toString(PList.size()));
+							    	Log.i("PLISTTEST","PList.get(i): " + PList.get(i));
+							    	
+							    	
 							    	Bitmap b = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_8888);
 							    	Canvas c = new Canvas(b);
 							    	c.drawBitmap(b, 0, 0, null);
@@ -161,14 +187,21 @@ public class MainActivity extends Activity {
 							    	textPaint.setAntiAlias(true);
 							    	textPaint.setTextSize(128.0F);
 							    	StaticLayout sl= new StaticLayout(text, textPaint, b.getWidth()-8, Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
+							    	//DynamicLayout dynamicText = new DynamicLayout(text, textPaint, b.getWidth()-8, Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
 							    	c.translate(6, 40);
 							    	sl.draw(c);
+							    	//dynamicText.draw(c);
 							    	
+							    	MeshComponent locationMesh = objectFactory.newTexturedSquare("HelloBitmap" + Integer.toString(i), b, 10);
+							    	locationMesh.addAnimation(new AnimationFaceToCamera(camera, 0.5f));
 							    	
-							    	x.setComp(objectFactory.newTexturedSquare("HelloBitMap", b, 10));
+							    	x.setComp(locationMesh);
+							    	//x.setComp(objectFactory.newTexturedSquare("HelloBitMap", b, 10));
 							    	
 							    	world.add(x);
 							    }
+							  //kill proc
+							    //android.os.Process.killProcess(android.os.Process.myPid());
 							    
 							   
 					}
@@ -177,23 +210,9 @@ public class MainActivity extends Activity {
 						Location newLocation = new Location("addLoc()");
 						newLocation.setLatitude(one);
 						newLocation.setLongitude(two);
+						newLocation.setAltitude(1000);
 						return newLocation;
 						
-					}
-					
-					public Bitmap textAsBitmap(String text, float textSize, int textColor) {
-					    Paint paint = new Paint();
-					    //paint.setTextSize
-					    paint.setTextSize(textSize);
-					    paint.setColor(textColor);
-					    paint.setTextAlign(Paint.Align.LEFT);
-					    int width = (int) (paint.measureText(text) + 0.5f); // round
-					    float baseline = (int) (-paint.ascent() + 0.5f); // ascent() is negative
-					    int height = (int) (baseline + paint.descent() + 0.5f);
-					    Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-					    Canvas canvas = new Canvas(image);
-					    canvas.drawText(text, 0, baseline, paint);
-					    return image;
 					}
 					
 					
@@ -204,18 +223,38 @@ public class MainActivity extends Activity {
 		});
 		setContentView(b);
 		
-		new GetPoints().execute();
-		for(Double entry: LonList){
-			Log.i("POINTS", "POINTFLAG" + entry.toString());
+		Log.d("EXTNAMES", "PList.size(): " + PList.size());
+		try {
+			new GetPoints().execute().get((long)10.0, TimeUnit.SECONDS);
+			//wait(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		for(String entry: PList){
+			Log.d("EXTNAMES", "EXTNAME: " + entry);
+		}
+		Log.d("EXTLATS", "LatList.size(): " + LatList.size());
+		for(Double entry: LatList){
+			Log.d("EXTLATS", "EXTLAT: " + entry);
+		}
+		
+		//kill proc
+		//android.os.Process.killProcess(android.os.Process.myPid());
 	}
 	
 	
 	//JSON **MIGHT NOT WORK**
 	private static String pt_url="http://people.clemson.edu/~myankou/php/EyeApp/points.php";
-	private static final String TAG_NAME = "Name";
+	private static final String TAG_NAME = "name";
 	//add tags for latitude and longitude and campus ID
-	ArrayList<String> PList;
+	//ArrayList<String> PList;
 	//ArrayList<Double> LatList, LonList;
 	//I guess use separate lists for each set of variables since the one implemented here only retreives names
 
@@ -225,7 +264,7 @@ public class MainActivity extends Activity {
 					@Override
 			        protected void onPreExecute() {
 			            super.onPreExecute();
-			            pDialog = new ProgressDialog(getApplicationContext());
+			            pDialog = new ProgressDialog(MainActivity.this);
 			            pDialog.setMessage("Retreiving points...");
 			            pDialog.setCancelable(false);
 			            pDialog.show();
@@ -245,19 +284,29 @@ public class MainActivity extends Activity {
 			                try {
 			                    JSONArray jsonObj = new JSONArray(ptStr);
 			                    PList = new ArrayList<String>();
-			                    PList.add("");
+			                    //PList.add("TEST");
 			                    
 			                    LatList = new ArrayList<Double>();
 			                    LonList = new ArrayList<Double>();
+			                  
+			                    Log.d("jsonObj.length()", Integer.toString(jsonObj.length()));
 			                    
+								
+			                    String strToAdd = "";
 								//add 3 more for loops for each set of variables
 			                    for (int i = 0; i < jsonObj.length(); i++) {
 			                        JSONObject c = jsonObj.getJSONObject(i);
-			                		PList.add(c.getString(TAG_NAME));
+			                        strToAdd = c.getString(TAG_NAME);
+			                        Log.d("strToAdd", "JSONSTRING: " + strToAdd);
+			                		PList.add(strToAdd);
 			                		LatList.add(c.getDouble("gps_lat"));
 			                		LonList.add(c.getDouble("gps_lon"));
 			                    }
 			                   // for (int i = 0; i < json)
+			                    //android.os.Process.killProcess(android.os.Process.myPid());
+			                    //this.
+			                    //Toast toast = Toast.makeText(getApplicationContext(), "jsonObj.length(): " + Integer.toString(jsonObj.length()) + "\n CONTENTS: " + PList.get(0), 10000);
+								//toast.show();
 			                    
 			                } catch (JSONException e) {
 			                    e.printStackTrace();
